393b3936d9427de9154879bd78a4b236f83ca991
Tanya Bansal <91904607+22tanyabansal@users.noreply.github.com>
2023-02-03 00:46:27 +0530

    Create Header.js

---
+++ src/Components/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+export const Header = () => {
+    return (
+    <div>
+        <h1 style={{ marginLeft: "20px", marginTop: "10px",alignItems:"center",color:"red"}}>Leadzen-Users</h1>
+    </div>
+);
+};
+export default Header;
